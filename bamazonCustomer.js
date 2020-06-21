59f9a50575c9c0d4678ff48e96d171f4cb933591
Diana Mayhugh <dianamayhugh@gmail.com>
2020-06-21 13:43:58 -0400

    correct message for not enough in stock

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -3,7 +3,6 @@ var inquirer = require("inquirer");
 const CheckboxPrompt = require("inquirer/lib/prompts/checkbox");
 require("dotenv").config();
 
-
 // create the connection information for the sql database
 var connection = mysql.createConnection({
   host: "localhost",
@@ -47,51 +46,53 @@ displayItemsForSale();
 //inquirer prompt
 function askProduct() {
   // query the database for all items being sold
-  connection.query("SELECT * FROM products", function(err, results) {
+  connection.query("SELECT * FROM products", function (err, results) {
     if (err) throw err;
     // once you have the items, prompt the user for which they'd like to bid on
-  inquirer
-    .prompt([
-      {
-        name: "item_id",
-        type: "number",
-        message: "What is the ID of the product you would like to buy?",
-      },
-      {
-        name: "quantity",
-        type: "number",
-        message: "How many units would you like to purchase?",
-      },
-    ])
-    .then(function (answer) {
-      //get the information of the chosen item
-      var chosenItem;
-      for (var i = 0; i < results.length; i++) {
-        if (results[i].item_id === answer.item_id) {
-          chosenItem = results[i];
+    inquirer
+      .prompt([
+        {
+          name: "item_id",
+          type: "number",
+          message: "What is the ID of the product you would like to buy?",
+        },
+        {
+          name: "quantity",
+          type: "number",
+          message: "How many units would you like to purchase?",
+        },
+      ])
+      .then(function (answer) {
+        //get the information of the chosen item
+        var chosenItem;
+        for (var i = 0; i < results.length; i++) {
+          if (results[i].item_id === answer.item_id) {
+            chosenItem = results[i];
+          }
         }
-      }
-      //determine if enough in stock
-      if (chosenItem.stock_quantity >= parseInt(answer.quantity)) {
-        
-        var updateStockQuantity =
-          "UPDATE products SET stock_quantity = " +
-          (chosenItem.stock_quantity - answer.quantity) +
-          " WHERE item_id = " +
-          answer.item_id;
+        //determine if enough in stock
+        if (chosenItem.stock_quantity >= parseInt(answer.quantity)) {
+          var updateStockQuantity =
+            "UPDATE products SET stock_quantity = " +
+            (chosenItem.stock_quantity - answer.quantity) +
+            " WHERE item_id = " +
+            answer.item_id;
 
-        connection.query(updateStockQuantity,function (error) {
+          connection.query(updateStockQuantity, function (error) {
             if (error) throw err;
             console.log("Order Fullfilled Successfully!");
-            console.log("Your total is $ " + chosenItem.price * answer.quantity);
+            console.log(
+              "Your total is $ " + chosenItem.price * answer.quantity
+            );
             askProduct();
-          }
-        );
-      } else {
-        //not enough in stock, so apologize and start over
-        console.log("Insufficient quantity, sorry we do not have enough " + results.product_name + " to complete your order.");
-        askProduct();
-      }
-    });
+          });
+        } else {
+          //not enough in stock, so apologize and start over
+          console.log(
+            "Insufficient quantity"
+          );
+          askProduct();
+        }
+      });
   });
 }
